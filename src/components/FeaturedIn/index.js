5d952e680d820e2f5b13df5144fa8065e51ae98f
iamwhitegod <kmbagwu12@gmail.com>
2023-05-22 10:50:12 +0100

    added link to the featuredIn section

--- src/components/FeaturedIn/index.js
+++ src/components/FeaturedIn/index.js
@@ -1,5 +1,6 @@
 import Section from "@/Layout/Section";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const FeaturedIn = () => {
@@ -9,27 +10,44 @@ const FeaturedIn = () => {
         <span className="featured-in__caption">As Featured in</span>
 
         <div className="featured-in__logos">
-          <Image
-            src={require("@/assets/img/vanguard@2x.png")}
-            alt={"Vanguard Logo"}
-          />
-          <Image
-            src={require("@/assets/img/accounteer@2x.png")}
-            alt={"Accounteer logo"}
-          />
-          <Image
-            src={require("@/assets/img/smepeaks@2x.png")}
-            alt={"SME peaks logo"}
-          />
-          <Image
-            src={require("@/assets/img/slush@2x.png")}
-            alt={"Slush logo"}
-          />
-          <Image
-            src={require("@/assets/img/techpoint@2x.png")}
-            alt={"Techpoint logo"}
-          />
-          <Image src={require("@/assets/img/bbc@2x.png")} alt={"BBC logo"} />
+          <Link href="https://www.vanguardngr.com/2022/08/gerocare-seeks-improved-care-for-elderly">
+            <Image
+              src={require("@/assets/img/vanguard@2x.png")}
+              alt={"Vanguard Logo"}
+            />
+          </Link>
+
+          <Link href="https://smepeaks.com/2018/05/21/gerocare-ais-rwanda">
+            <Image
+              src={require("@/assets/img/smepeaks@2x.png")}
+              alt={"SME peaks logo"}
+            />
+          </Link>
+
+          <Link href="https://accounteer.com/en/">
+            <Image
+              src={require("@/assets/img/accounteer@2x.png")}
+              alt={"Accounteer logo"}
+            />
+          </Link>
+
+          <Link href="https://smepeaks.com/2019/01/09/dofrey-ebi-gerocare-slush-gia-experience-feature">
+            <Image
+              src={require("@/assets/img/slush@2x.png")}
+              alt={"Slush logo"}
+            />
+          </Link>
+
+          <Link href="https://techpoint.africa/2017/06/12/gerocare-profile">
+            <Image
+              src={require("@/assets/img/techpoint@2x.png")}
+              alt={"Techpoint logo"}
+            />
+          </Link>
+
+          <Link href="https://www.bbc.com/news/av/business-52667793">
+            <Image src={require("@/assets/img/bbc@2x.png")} alt={"BBC logo"} />
+          </Link>
         </div>
       </div>
     </Section>
